ec409b799055c6f26ffbfdb731f333d8783b9b0a
duck <trvuduc@gmail.com>
2022-07-26 13:00:18 +0700

    Added support for Vietnamese localization ('vi')

---
+++ lib/src/l10n/month_year_picker_localizations_vi.dart
@@ -0,0 +1,15 @@
+import 'month_year_picker_localizations.dart';
+
+/// The translations for English (`en`).
+class MonthYearPickerLocalizationsVi extends MonthYearPickerLocalizations {
+  MonthYearPickerLocalizationsVi([String locale = 'vi']) : super(locale);
+
+  @override
+  String get helpText => 'Chọn tháng';
+
+  @override
+  String get okButtonLabel => 'OK';
+
+  @override
+  String get cancelButtonLabel => 'Hủy';
+}
