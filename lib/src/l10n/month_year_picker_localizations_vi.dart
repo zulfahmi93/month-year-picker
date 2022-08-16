67cb1c595b7926925bee5dc8519554367fa837d1
duck <trvuduc@gmail.com>
2022-08-16 17:45:05 +0700

    Correct typo

--- lib/src/l10n/month_year_picker_localizations_vi.dart
+++ lib/src/l10n/month_year_picker_localizations_vi.dart
@@ -1,6 +1,6 @@
 import 'month_year_picker_localizations.dart';
 
-/// The translations for English (`en`).
+/// The translations for Vietnamese (`vi`).
 class MonthYearPickerLocalizationsVi extends MonthYearPickerLocalizations {
   MonthYearPickerLocalizationsVi([String locale = 'vi']) : super(locale);
 
