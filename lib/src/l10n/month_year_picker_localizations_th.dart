f4f584f1dc06469914f142badc2b3062588735e4
Sitthipong Peamoontree <62597051+STP-PMT@users.noreply.github.com>
2022-10-09 22:49:54 +0700

    Create month_year_picker_localizations_th.dart
    translations for Thai

---
+++ lib/src/l10n/month_year_picker_localizations_th.dart
@@ -0,0 +1,15 @@
+import 'month_year_picker_localizations.dart';
+
+/// The translations for Thai (`th`).
+class MonthYearPickerLocalizationsTh extends MonthYearPickerLocalizations {
+  MonthYearPickerLocalizationsTh([String locale = 'th']) : super(locale);
+
+  @override
+  String get helpText => 'เลือก เดือน/ปี';
+
+  @override
+  String get okButtonLabel => 'ตกลง';
+
+  @override
+  String get cancelButtonLabel => 'ยกเลิก';
+}
