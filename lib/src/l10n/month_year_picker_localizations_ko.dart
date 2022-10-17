7f1cf158c1f6fd4e6bffa56b04a5b7244445cb90
강현호 <hawk0530@mint-soft.com>
2022-10-17 20:22:37 +0900

    ko add

---
+++ lib/src/l10n/month_year_picker_localizations_ko.dart
@@ -0,0 +1,15 @@
+import 'month_year_picker_localizations.dart';
+
+/// The translations for English (`ko`).
+class MonthYearPickerLocalizationsEn extends MonthYearPickerLocalizations {
+  MonthYearPickerLocalizationsEn([String locale = 'ko']) : super(locale);
+
+  @override
+  String get helpText => '년월 선택';
+
+  @override
+  String get okButtonLabel => '확인';
+
+  @override
+  String get cancelButtonLabel => '취소';
+}
