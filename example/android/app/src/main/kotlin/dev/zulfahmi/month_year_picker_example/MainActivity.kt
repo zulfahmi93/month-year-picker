124c6bc3a120b46499b6a05cf36bcbcef36f3174
Zulfahmi Ahmad <zulfahmi93@outlook.com>
2025-05-22 17:20:57 +0800

    chore: Re-generate example runners.

--- example/android/app/src/main/kotlin/dev/zulfahmi/month_year_picker_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/dev/zulfahmi/month_year_picker_example/MainActivity.kt
@@ -2,5 +2,4 @@ package dev.zulfahmi.month_year_picker_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
